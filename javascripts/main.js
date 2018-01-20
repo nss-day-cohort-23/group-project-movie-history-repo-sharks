20ea4ca7e036cab0cf9864afa8af21b2965580e9
Slade Utley <slade.utley@gmail.com>
2018-01-20 16:31:21 -0600

    wrote getMovies and delete movie function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 console.log("hello");
+
+const fbURL = `https://reposharks.firebaseio.com`;
+const $ = require(`jquery`);
+const firebase = require(`./config/fb-config`);
+const auth = require('./user-factory');
+
+function getMovies(uid) {
+    return new Promise ((resolve, reject) => {
+        $.ajax({
+            url: `${fbURL}.json?orderBy="uid"&equalTo="${uid}"`
+        })
+            .done(movies => {
+                resolve(movies);
+            })
+            .fail(error => {
+                console.log("somethings gone wrong", error.statusText);
+                reject(error);
+        });
+    });
+}
+
+function deleteMovie(id) {
+    return new Promise ((resolve, reject) => {
+        $.ajax({
+            url: `${fbURL}/${id}.json`,
+        })
+            .done(data => {
+                resolve(data);
+            })
+            .fail(error => {
+                console.log("somethings gone wrong", error.statusText);
+                reject(error);
+        });
+    });
+}
+
