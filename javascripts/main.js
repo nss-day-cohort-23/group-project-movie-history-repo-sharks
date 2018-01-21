d6355e076776a4883a03f1a6ffc60d6c80cc7c96
Slade Utley <slade.utley@gmail.com>
2018-01-21 11:05:13 -0600

    started firebase data module

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,16 +2,33 @@
 
 const $ = require(`jquery`);
 
-let movieDB = require(`./config/mdb-creds`);
-let movies = movieDB.apiKey;
-console.log(movies);
 
-function getMovies(searchedMovies) {
-    
+//Movie Database Module
+let movieDB = require(`./config/mdb-creds`);
+let moviesAPIKey = movieDB.apiKey;
+let moviesURL = movieDB.authDomain;
+
+function getMovies(searchedMovie) {
+    return new Promise ((resolve, reject) => {
+        $.ajax({
+            url: `${moviesURL}${moviesAPIKey}&query=${searchedMovie}`
+        })
+            .done(data => {
+            resolve(data);
+        })
+            .fail(error => {
+                console.log("somethings gone wrong", error.statusText);
+                reject(error);
+        });
+    });
 } 
 
+// console.log(getMovies("forrest gump"));
+//GOT THIS MESSAGE
+// The SSL certificate used to load resources from https://api.themoviedb.org will be distrusted in M70. Once distrusted, users will be prevented from loading these resources. See https://g.co/chrome/symantecpkicerts for more information.
 
 
+//Firebase Data Module
 const fbURL = `https://reposharks.firebaseio.com`;
 const firebase = require(`./config/fb-config`);
 const auth = require('./user-factory');
@@ -59,3 +76,4 @@ function addMovie(movie) {
     });
 }
 
+//User log in and log out
