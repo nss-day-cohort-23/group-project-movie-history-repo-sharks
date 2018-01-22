84cbf59c0d7af34963b6328452c7416706f9d95f
Slade Utley <slade.utley@gmail.com>
2018-01-22 11:57:03 -0600

    started new output to dom module that will be called in controller to display searched movie results

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -7,22 +7,51 @@
 //USER SEARCH MODULE
 //probably gonna have to have a if "#yourMovies" is checked, run function that gets your movies, else run get new movies function
 let movieFactory = require('./movie-factory');
+let outputToDom = require('./outputToDom');
 
 let userText = document.getElementById("search");
+let movieObject = {
+    movieId: "",
+    title: "",
+    img: "",
+    yearReleased: "",
+    cast: "",
+    userId: "",
+    watchlist: false
+};
+
+//want to get movie id from search results
+
+// let movieIdArray = [];
+
+// function getMoviesId(searchedMovie) {
+//     let keys = Object.keys(searchedMovie);
+//     keys.forEach(key => {
+//         searchedMovie[key].id = key;
+//         movieIdArray.push(searchedMovie[key]);
+//         console.log("movieIdArray", movieIdArray);
+//     });
+// }
 
 module.exports.pressingEnter = (searchedMovie) => {
     userText.addEventListener('keypress', function (e) {
-    var key = e.keyCode;
+        var key = e.keyCode;
         if (key === 13) {
             console.log("enter key working");
             searchedMovie = userText.value;
             movieFactory.getMovies(searchedMovie)
             .then(function(movieData) {
-                console.log("movieData", movieData);
+                //movieData.forEach(movie => {
+                    // getMoviesId(searchedMovie);
+                    console.log("movieObject", movieObject);
+                    console.log("movieData", movieData);
+
+                // });
             });
             userText.value = "";
         }
     });
 };
+
 
 
