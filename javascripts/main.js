9949c5a9fa981dbc20e9b095d25ff2ede4a267bb
Slade Utley <slade.utley@gmail.com>
2018-01-23 13:08:47 -0600

    began writing function to display users watched movies

--- javascripts/main.js
+++ javascripts/main.js
@@ -54,26 +54,42 @@ $(document).on("click", "#watchlist", function(){
         alert("Please log in to continue..");
 });
 
+function listUsersMovies(movieData) {
+    console.log("user movies", movieData);
+    let userMovieArr = [];
+    let keys = Object.keys(movieData);
+    keys.forEach(key => {
+        movieData[key].id = key;
+        userMovieArr.push(movieData[key]);
+    });
+    console.log("user movie arr: ", userMovieArr);
+}
+
+function displayUserMovies(uid) {
+    movieFactory.getUsersMovies(uid)
+    .then(movieData => {
+        listUsersMovies(movieData);
+    });
+}
+
 $(document).on("click", "#showWatched", function(){
-    console.log('hello');
     let currentUser = firebase.auth().currentUser;
     console.log('currentUser',currentUser);
     if (currentUser != undefined) {
         console.log('pressed watched button');
-
-        //controller.addMovieObjectToWatchlist(movieId, currentUser.uid);
-        //call function here
+        console.log("current user", currentUser);
+        displayUserMovies(currentUser.uid);
+        // controller.getUsersWatchedMovies();
     } else
         alert("Please log in to continue..");
 });
 
 $(document).on("click", "#showFavorites", function(){
-    console.log('hello');
     let currentUser = firebase.auth().currentUser;
     console.log('currentUser',currentUser);
     if (currentUser != undefined) {
         console.log('pressed favorites button');
-
+        // controller.
         //controller.addMovieObjectToWatchlist(movieId, currentUser.uid);
         //call function here
     } else
